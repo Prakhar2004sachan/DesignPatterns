821477a91714f7b6c11386b7593b1e92d3bc2275
Prakhar2004sachan <157094243+Prakhar2004sachan@users.noreply.github.com>
2026-09-19 22:18:09 +0530

    factory pattern js implementation

--- 03_Factory_Method/js/src/index.js
+++ 03_Factory_Method/js/src/index.js
@@ -1,8 +1,83 @@
 // 03_Factory_Method Pattern - JavaScript Implementation
 
+// Product
+class Document {
+  open() {
+    throw new Error("Abstract open");
+  }
+
+  edit() {
+    throw new Error("Abstract edit");
+  }
+
+  save() {
+    throw new Error("Abstract save");
+  }
+}
+
+class WordDocument extends Document {
+  open() {
+    console.log("Word : open");
+  }
+
+  edit() {
+    console.log("Word : edit");
+  }
+
+  save() {
+    console.log("Word: save");
+  }
+}
+
+class PdfDocument extends Document {
+  open() {
+    console.log("Pdf : open");
+  }
+
+  edit() {
+    console.log("Pdf : edit");
+  }
+
+  save() {
+    console.log("Pdf: save");
+  }
+}
+
+// Creator
+class DocumentEditor {
+  createDocument() {
+    throw new Error("Abstract Create Document");
+  }
+
+  run(){
+    const doc = this.createDocument();
+    doc.open();
+    doc.edit();
+    doc.save();
+  }
+}
+
+class WordEditor extends DocumentEditor {
+  createDocument(){
+    return new WordDocument();
+  }
+}
+
+class PdfEditor extends DocumentEditor {
+  createDocument(){
+    return new PdfDocument();
+  }
+}
+
+
 function main() {
   console.log("=== [JavaScript] 03_Factory_Method ===");
-  // TODO: Implement 03_Factory_Method pattern in JavaScript
+
+  const wordEditor = new WordEditor();
+  const pdfEditor = new PdfEditor();
+
+  wordEditor.run();
+  pdfEditor.run();
 }
 
 main();
