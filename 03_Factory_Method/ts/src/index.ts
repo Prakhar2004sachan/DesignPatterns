605057e191bbba639c8a34ac5690501aabd95b6f
Prakhar2004sachan <157094243+Prakhar2004sachan@users.noreply.github.com>
2026-09-19 22:28:52 +0530

    Factory pattern : typescript implementation class architecture

--- 03_Factory_Method/ts/src/index.ts
+++ 03_Factory_Method/ts/src/index.ts
@@ -1,8 +1,69 @@
 // 03_Factory_Method Pattern - TypeScript Implementation
 
+abstract class Document {
+  abstract open() : void;
+  abstract edit() : void;
+  abstract save() : void;
+}
+
+class WordDocument extends Document {
+  open(): void {
+    console.log("Word : open");
+  }
+
+  edit(): void {
+    console.log("Word : edit");
+  }
+
+  save(): void {
+    console.log("Word : save");
+  }
+}
+
+class PdfDocument extends Document {
+  open(): void {
+    console.log("Pdf : open");
+  }
+
+  edit(): void {
+    console.log("Pdf : edit");
+  }
+
+  save(): void {
+    console.log("Pdf : save");
+  }
+}
+
+abstract class DocumentEditor {
+  abstract createDocument() : Document;
+  run() : void {
+    const doc = this.createDocument();
+    doc.open();
+    doc.edit();
+    doc.save();
+  };
+}
+
+class WordEditor extends DocumentEditor {
+  createDocument(): Document {
+    return new WordDocument();
+  }
+}
+
+class PdfEditor extends DocumentEditor {
+  createDocument(): Document {
+    return new PdfDocument();
+  }
+}
+
 function main(): void {
   console.log("=== [TypeScript] 03_Factory_Method ===");
-  // TODO: Implement 03_Factory_Method pattern in TypeScript
+
+  const wordEditor = new WordEditor();
+  const pdfEditor = new PdfEditor();
+
+  wordEditor.run();
+  pdfEditor.run();
 }
 
 main();
