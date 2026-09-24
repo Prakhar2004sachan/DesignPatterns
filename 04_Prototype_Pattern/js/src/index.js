1f0228506c3d00f69fb066a38b0e46e57da7336a
Prakhar2004sachan <157094243+Prakhar2004sachan@users.noreply.github.com>
2026-09-24 15:17:46 +0530

    shallow clone js implementation of protoype pattern

--- 04_Prototype_Pattern/js/src/index.js
+++ 04_Prototype_Pattern/js/src/index.js
@@ -1,5 +1,12 @@
 // 04_Prototype_Pattern Pattern - JavaScript Implementation
 
+// Shallow clone - spread / object.assign
+const originalObj = {name: "periods", tags: ["new", "old"]};
+const shallowObj = {...originalObj};
+console.log(originalObj);
+console.log(shallowObj);
+
+
 function main() {
   console.log("=== [JavaScript] 04_Prototype_Pattern ===");
   // TODO: Implement 04_Prototype_Pattern pattern in JavaScript
